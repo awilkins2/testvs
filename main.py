97b45d900d95304629093afc167928d25bcd0013
Aaron <aaronwilkin25@gmail.com>
2023-10-04 09:15:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#data abstraction - when you need to simplify data 
+# from something more complex
+#an example of data abstraction is when I use a list to simplify data
+#instead of writing same code repeadetly 
+# if you have list of names, simplify by using a list
